1353554f88d7edf4ce7e74495b6c028aff113f33
Josh Davis <joshdavis739@gmail.com>
2020-02-11 21:53:14 +0000

    Add in blog and about pages; make nice breadcrumbs

--- WebUI/src/app/app-routing.module.ts
+++ WebUI/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { AboutMeComponent } from './modules/about-me/about-me.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { BlogComponent } from './modules/blog/blog.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'blog', component: BlogComponent }
 ];
 
 @NgModule({
--- WebUI/src/app/app.module.ts
+++ WebUI/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './modules/header/header.component';
 import { HomeComponent } from './modules/home/home.component';
+import { AboutMeModule } from './modules/about-me/about-me.module';
+import { ContactModule } from './modules/contact/contact.module';
+import { BlogModule } from './modules/blog/blog.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { HomeComponent } from './modules/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AboutMeModule,
+    ContactModule,
+    BlogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
