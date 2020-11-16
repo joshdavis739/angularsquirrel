81e4a8751fe7d3015a9a50c696ac6990f2e1a857
Josh Davis <joshdavis739@gmail.com>
2020-11-16 18:05:32 +0000

    Add firebase support

--- WebUI/e2e/src/app.e2e-spec.ts
+++ WebUI/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,6 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
+import { protractor } from 'protractor/built/ptor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -9,6 +11,38 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to WebUI!');
+    expect(page.getTitleText()).toEqual('Welcome to Angular Squirrel');
+  });
+
+  it('should have a navbar', () => {
+    // Arrange
+    page.navigateTo();
+    // Act
+    var navbar = page.getNavBar();
+    browser.wait(protractor.ExpectedConditions.visibilityOf(navbar), 1000);
+    // Assert
+    expect(navbar).toBeDefined();
+  });
+
+  it('should have a home button on the navbar', () => {
+    page.navigateTo();
+    var homeButton = page.getHomeButton();
+    browser.wait(protractor.ExpectedConditions.visibilityOf(homeButton), 1000);
+    expect(homeButton.getText()).toBeDefined();
+  });
+
+  it('should have a blog button on the navbar', () => {
+    page.navigateTo();
+    var blogButton = page.getBlogButton();
+    browser.wait(protractor.ExpectedConditions.visibilityOf(blogButton), 1000);
+    expect(blogButton.getText()).toBe('BLOG');
+  });
+
+  it('should navigate to /blog on clicking the blog button', () => {
+    page.navigateTo();
+    var blogButton = page.getBlogButton();
+    browser.wait(protractor.ExpectedConditions.visibilityOf(blogButton), 1000);
+    blogButton.click()
+    expect(browser.driver.getCurrentUrl()).toContain('/blog');
   });
 });
--- WebUI/e2e/src/app.po.ts
+++ WebUI/e2e/src/app.po.ts
@@ -1,11 +1,23 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder, promise } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
+  public navigateTo(): promise.Promise<any> {
     return browser.get('/');
   }
 
-  getTitleText() {
+  public getTitleText(): promise.Promise<string> {
     return element(by.css('app-root h1')).getText();
+  }
+
+  public getNavBar(): ElementFinder {
+    return element(by.tagName('nav'));
+  }
+
+  public getHomeButton(): ElementFinder {
+    return this.getNavBar().element(by.linkText('HOME'));
+  }
+
+  public getBlogButton(): ElementFinder {
+    return this.getNavBar().element(by.linkText('BLOG'));
   }
 }
