e06c46f1af095483bfbc6b12a761eb63aaf838e4
Josh Davis <joshdavis739@gmail.com>
2021-01-19 21:34:56 +0000

    Change icon; tidy up

--- WebUI/src/app/shared/shared.module.ts
+++ WebUI/src/app/shared/shared.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CollapsibleSectionComponent } from './collapsible-section/collapsible-section.component';
 import { CardComponent } from './card/card.component';
 import { RouterModule } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
-  declarations: [CollapsibleSectionComponent, CardComponent],
+  declarations: [CardComponent],
   imports: [
     CommonModule,
     RouterModule,
     MarkdownModule.forChild()
   ],
-  exports: [CollapsibleSectionComponent, CardComponent]
+  exports: [CardComponent]
 })
 export class SharedModule { }
