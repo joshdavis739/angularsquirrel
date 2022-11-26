2f1438b053defb79c44bcdc08f38b38759ebb6bc
Josh Davis <josh.davis@enable.com>
2022-11-26 14:15:41 +0000

    Remove old features

--- WebUI/src/app/modules/about-me/about-me.module.ts
+++ WebUI/src/app/modules/about-me/about-me.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AboutMeComponent } from './about-me.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ExternalLinkComponent } from './external-link/external-link.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AboutMeComponent, ExternalLinkComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class AboutMeModule { }
--- WebUI/src/app/modules/blog/blog.module.ts
+++ WebUI/src/app/modules/blog/blog.module.ts
@@ -7,26 +7,11 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { MarkdownModule } from 'ngx-markdown';
 import { BlogApiService } from './blog-api.service';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: BlogComponent,
-    children:
-    [
-    ]
-  },
-  {
-    path: 'blog/:articleIdentifier',
-    component: BlogArticleComponent
-  }
-];
-
 @NgModule({
   declarations: [BlogComponent, BlogArticleComponent],
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule.forChild(routes),
     MarkdownModule.forChild()
   ],
   providers: [BlogApiService]
